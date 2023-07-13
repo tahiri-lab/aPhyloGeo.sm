7a1a483f6171eee4222f7efb72986a6ab142cdc2
Wanlin Li <53668767+WanlinLi2021@users.noreply.github.com>
2023-07-13 17:49:56 -0500

    step 1 and step 2

---
+++ step1_alignment/downloadSeq.py
@@ -0,0 +1,33 @@
+from io import StringIO
+from Bio import SeqIO
+from Bio import Entrez
+import os
+import pandas as pd
+
+
+def downFromNCBI(db_type, accession_list, file_name):
+    Entrez.email = "806981384wl@gmail.com"  # Always tell NCBI who you are
+
+    if not os.path.isfile(file_name):
+        # Downloading...
+        net_handle = Entrez.efetch(
+            db=db_type, id=accession_list, rettype="fasta", retmode="text"
+        )
+        out_handle = open(file_name, "w")
+        out_handle.write(net_handle.read())
+        out_handle.close()
+        net_handle.close()
+        print("Saved")
+
+
+df = pd.read_csv("data.csv")
+accession_list = set(df.V_Accession_full)
+# accession_list = set(df.Host_Accession)
+print(accession_list)
+print(len(accession_list))
+
+# file_name = "seq_full_virus.fasta"
+# file_name = "seq_cytb_bat.fasta"
+
+
+# downFromNCBI("nucleotide", accession_list, file_name)
